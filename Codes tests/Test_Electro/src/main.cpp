46449a90f32e22829e2f9ff71c099af726385672
Aimee <stewarteaimee@gmail.com>
2024-07-04 10:39:20 -0400

    test_electro test

--- Codes tests/Test_Electro/src/main.cpp
+++ Codes tests/Test_Electro/src/main.cpp
@@ -3,17 +3,19 @@
 #include <ArduinoJson.h>
 
 ArduinoX AX_;
+uint8_t MAGPIN = 32;
 
 void setup() {
   Serial.begin(115200);
   AX_.init();
-  pinMode(LED_BUILTIN, OUTPUT);
+  //hey i changed some junkkkk
+  pinMode(MAGPIN, OUTPUT);
 }
 
 void loop() {
   // Ça fait flasher la led sur le Arduino
+
+  digitalWrite(MAGPIN,HIGH);
   delay(1000);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(MAGPIN,LOW);
 }
