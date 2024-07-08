04ead57e958fe12f37ad38eed17e02e198fbc797
Émile Raymond <raye1901@usherbrooke.ca>
2024-07-08 10:18:14 -0400

    Merge branch 'main' of https://github.com/Aiglechauve99/SherPendule_Repo

---
+++ Codes tests/Test_PID2/src/main.cpp
@@ -0,0 +1,38 @@
+#include <Arduino.h>
+#include <LibS3GRO.h>
+#include <ArduinoJson.h>
+#include "PID_A.h"
+
+ArduinoX AX_;
+MotorControl MC_;
+
+void setup() {
+  Serial.begin(115200);
+  AX_.init();
+  MC_.init(8,7);
+}
+double PotentioDeg(){
+  double deg = analogRead(A5)/4.6;
+  return deg;
+}
+double EncodeurPos(){
+  double pos = AX_.readEncoder(0)*0.0001246;
+  return pos;
+}
+void loop() {
+    PID_A pidMoteur(2, 0.5, 0, 0.01); 
+    PID_A pidPendule(0, 0, 0, 0.01); 
+    double positionRequis = 1.0, angleRequis = 90;
+    double VitesseMoteur = 0; 
+    for (int i = 0; i < 1000; ++i) { 
+      double correctionMoteur = pidMoteur.calculsPID(positionRequis, EncodeurPos());
+      double correctionPendule = pidPendule.calculsPID(angleRequis, PotentioDeg());
+
+      double commandeVitesseMoteur = correctionMoteur + correctionPendule;
+
+      VitesseMoteur = commandeVitesseMoteur;
+      AX_.setMotorPWM(0, VitesseMoteur);
+      Serial.println(EncodeurPos());
+      }
+}
+
---
+++ Codes tests/test_Moteur/src/PID_A.h
@@ -0,0 +1,17 @@
+#ifndef PID_A_H
+#define PID_A_H
+
+class PID_A {
+private:
+    double kp; // proportionnel
+    double ki; // intégral
+    double kd; // dérivé
+    double erreur_precedente;
+    double integrale; // somme des erreurs
+    double temps_echantillonnage;
+
+public:
+    PID_A(double kp, double ki, double kd, double temps_echantillonnage);
+    double calculsPID(double valeur_requis, double valeur_actuelle);
+};
+#endif
