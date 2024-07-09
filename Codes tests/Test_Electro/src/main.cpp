9da5e75aefa2c4d279c44d4355bb03ab3d38e071
Aimee <stewarteaimee@gmail.com>
2024-07-09 15:20:32 -0400

    test electro avec good pin

--- Codes tests/Test_Electro/src/main.cpp
+++ Codes tests/Test_Electro/src/main.cpp
@@ -5,17 +5,24 @@
 ArduinoX AX_;
 uint8_t MAGPIN = 32;
 
+
 void setup() {
   Serial.begin(115200);
   AX_.init();
   
   pinMode(MAGPIN, OUTPUT);
+ 
 }
 
 void loop() {
   // Ça fait flasher la led sur le Arduino
 
   digitalWrite(MAGPIN,HIGH);
+  
+  Serial.println("electro on high");
   delay(5000);
   digitalWrite(MAGPIN,LOW);
+  
+  Serial.println("electro on low");
+  delay(5000);
 }
