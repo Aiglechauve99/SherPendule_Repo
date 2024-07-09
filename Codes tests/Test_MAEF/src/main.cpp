6124d93443dc71163ee4217785739de872a0e51c
Aimee <stewarteaimee@gmail.com>
2024-07-09 15:21:16 -0400

    Test de machine a etat

--- Codes tests/Test_MAEF/src/main.cpp
+++ Codes tests/Test_MAEF/src/main.cpp
@@ -4,6 +4,48 @@
 
 ArduinoX AX_;
 
+enum State {READY,PICK,APPROACH,SWING,OBSTACLE,STABILISE,DROP,RETURN};
+
+/*
+enum State next_state(enum State current)
+{
+  switch(current){
+    case READY: return PICK;
+    case PICK: return APPROACH;
+    case APPROACH: return SWING;
+    case SWING: return OBSTACLE;
+    case OBSTACLE: return STABILISE;
+
+  }
+}*/
+
+switch(enum State transition){
+  case READY:
+    //initialize the board and pins
+    break;
+  case PICK:
+    //Activate magnet
+    break;
+  case APPROACH:
+    //move to position defined
+   break;
+  case SWING:
+    //oscilolate pendulum until desired anclge and frequency 
+  case OBSTACLE:
+   //pass the obstacle and get to the end place
+   break;
+  case STABILISE:
+    //stabilise the pendulum for drop off
+    break;
+  case DROP:
+    //
+}
+
+
+
+
+
+
 void setup() {
   Serial.begin(115200);
   AX_.init();
